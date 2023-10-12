4789018b700f75a0d825cc594da87fe04a7fa011
vanessacristin <cristinvanessa6@gmail.com>
2023-10-12 14:33:20 +0700

    percobaan 1

--- Pemilihan2Percobaan128.java
+++ Pemilihan2Percobaan128.java
@@ -8,11 +8,17 @@ public class Pemilihan2Percobaan128 {
         tahun = input28.nextInt();
 
     if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0 || (tahun % 400) == 0)
+        if (tahun % 100 != 0) {
+        if (tahun % 400 == 0) {
             System.out.println("Tahun Kabisat");
-        else
+        }  else {
             System.out.println("Bukan Tahun Kabisat");
-    } else
+        }
+        } else {
+            System.out.println("Tahun Kabisat");
+        }
+    } else{
         System.out.println("Bukan Tahun Kabisat");
     }
+}
 }
