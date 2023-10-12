dbf7a1cfb04897ffb36e598cf3ed017742dff291
vanessacristin <cristinvanessa6@gmail.com>
2023-10-12 11:50:47 +0700

    jobsheet 6

--- Pemilihan2Percobaan128.java
+++ Pemilihan2Percobaan128.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 public class Pemilihan2Percobaan128 {
     public static void main(String [] args) {
         Scanner input28 = new Scanner(System.in);
+
+        int tahun;
         System.out.print("Masukkan Tahun: ");
-        int tahun = input28.nextInt();
-        String hasil;
+        tahun = input28.nextInt();
+
     if ((tahun % 4) == 0) {
         if ((tahun % 100) != 0)
             System.out.println("Tahun Kabisat");
