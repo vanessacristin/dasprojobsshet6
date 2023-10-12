d528cce2f6689cb0ffe5e4f649c6be28f45889ee
vanessacristin <cristinvanessa6@gmail.com>
2023-10-12 12:19:25 +0700

    percobaan2

--- Pemilihan2Percobaan228.java
+++ Pemilihan2Percobaan228.java
@@ -17,12 +17,16 @@ public class Pemilihan2Percobaan228 {
         
     if (totalSudut == 180) {
         if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90))
-            System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-        else
-            System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-    } else
-        System.out.println("Bukan Segitiga");
-
+            System.out.println("Segitiga tersebut adalah Segitiga Siku-siku");
+        else if((sudut1 == sudut2) && (sudut2 == sudut3)){
+            System.out.println("Segitiga tersebut adalah segitiga Sama Sisi");
+        } else if((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3))
+            System.out.println("Segitiga tersebut adalah Segitiga Sama Kaki");
+            else 
+                System.out.println("Segitiga tersebut adalah Segitiga Sembarang");
+            } else{
+                System.out.println("Bukan Segitiga");
+        }
+        }
 
     }
-}
